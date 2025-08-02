1f158ca8ad0bf83ea4a6568afd385683bcc64e96
Lakshmi N <Lakshmi N@Ak.mshome.net>
2025-08-02 12:25:23 +0530

    prac modify

--- trial/src/main/java/pac/prac.java
+++ trial/src/main/java/pac/prac.java
@@ -1,9 +1,15 @@
 package pac;
 
+import org.openqa.selenium.support.FindBy;
+
 public class prac {
 
 	public void prac()
 	{
 		System.out.println("hi");
+		
+		System.out.println("bye");
+		
+		System.out.println("mummy");
 	}
 }
